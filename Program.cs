f5a4830da47229f978d09837507a8f22b40a21e9
Alexey <aav8402@mail.ru>
2022-09-17 16:02:19 +0300

    Zadacha pro KVADRAT

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и
+// выдает его квадрат (число умноженное само на себя).
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num * num;
+Console.WriteLine("Результат " + result);
+
+if (num == result)
+{
+    Console.WriteLine($"Не может такого быть!!!!");
+}
+else
+{
+    Console.WriteLine($"Да");
+}
